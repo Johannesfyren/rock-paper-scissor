3aeb49474cb3220433dab407a39e8424e84f14ad
JohannesFyren <hergaardjohannes@gmail.com>
2024-03-13 19:13:06 +0100

    Add consitions for of who wins computer or player

--- js.js
+++ js.js
@@ -9,7 +9,34 @@ function getComputerChoice() { //Computer generates an answer
         case 2: return "scissor"
         break;
     }
-        
+}
+
+function playGame(playerSelected, computerSelected){
+    const playerSelection = playerSelected.toLowerCase();
+    const computerSelection = computerSelected.toLowerCase();
+
+    if (playerSelection == computerSelection){ // TIE
+        return `That's a tie! You both chose ${computerSelection}`;
+    }
+
+    if (playerSelection == "rock" && computerSelection == "paper"){ //if statement to determine rock/paper
+        return `Computer won! ${computerSelection} beats ${playerSelection}`;
+    } else if(playerSelection == "paper" && computerSelection == "rock"){
+        return `Player won! ${playerSelection} beats ${computerSelection}`;
+    }
+
+    if (playerSelection == "paper" && computerSelection == "scissor"){ //if statement to determine scissor/paper
+        return `Computer won! ${computerSelection} beats ${playerSelection}`;
+    } else if(playerSelection == "scissor" && computerSelection == "paper"){
+        return `Player won! ${playerSelection} beats ${computerSelection}`;
+    }
+
+    if (playerSelection == "scissor" && computerSelection == "rock"){ //if statement to determine scissor/rock
+        return `Computer won! ${computerSelection} beats ${playerSelection}`;
+    } else if(playerSelection == "rock" && computerSelection == "scissor"){
+        return `Player won! ${playerSelection} beats ${computerSelection}`;
+    }
 
 }
-console.log(getComputerChoice());
+console.log(playGame("scissOr",getComputerChoice()));
+
