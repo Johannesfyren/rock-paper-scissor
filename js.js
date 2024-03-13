4c3c17c01ee6dc907bdb5842eb65eeabcae264d4
JohannesFyren <hergaardjohannes@gmail.com>
2024-03-13 19:44:03 +0100

    Add game mechanism to play 1 game of 5 rounds and determine a winner

--- js.js
+++ js.js
@@ -11,32 +11,75 @@ function getComputerChoice() { //Computer generates an answer
     }
 }
 
-function playGame(playerSelected, computerSelected){
+function playRound(playerSelected, computerSelected){
     const playerSelection = playerSelected.toLowerCase();
     const computerSelection = computerSelected.toLowerCase();
+    
 
     if (playerSelection == computerSelection){ // TIE
-        return `That's a tie! You both chose ${computerSelection}`;
+        
+        console.log(`That's a tie! You both chose ${computerSelection}`);
+        return 2;
     }
 
     if (playerSelection == "rock" && computerSelection == "paper"){ //if statement to determine rock/paper
-        return `Computer won! ${computerSelection} beats ${playerSelection}`;
+        
+        console.log(`Computer won! ${computerSelection} beats ${playerSelection}`);
+        return 0; //Computer won
     } else if(playerSelection == "paper" && computerSelection == "rock"){
-        return `Player won! ${playerSelection} beats ${computerSelection}`;
+        
+        console.log(`Player won! ${playerSelection} beats ${computerSelection}`);
+        return 1; //Player won
     }
 
     if (playerSelection == "paper" && computerSelection == "scissor"){ //if statement to determine scissor/paper
-        return `Computer won! ${computerSelection} beats ${playerSelection}`;
+        
+        console.log(`Computer won! ${computerSelection} beats ${playerSelection}`);
+        return 0; //Computer won
     } else if(playerSelection == "scissor" && computerSelection == "paper"){
-        return `Player won! ${playerSelection} beats ${computerSelection}`;
+        
+        console.log(`Player won! ${playerSelection} beats ${computerSelection}`);
+        return 1; //Player won
     }
 
     if (playerSelection == "scissor" && computerSelection == "rock"){ //if statement to determine scissor/rock
-        return `Computer won! ${computerSelection} beats ${playerSelection}`;
+       
+        console.log(`Computer won! ${computerSelection} beats ${playerSelection}`);
+        return 0; //Computer won
     } else if(playerSelection == "rock" && computerSelection == "scissor"){
-        return `Player won! ${playerSelection} beats ${computerSelection}`;
+        console.log(`Player won! ${playerSelection} beats ${computerSelection}`);
+        return 1; //Player won
+        
     }
 
 }
-console.log(playGame("scissOr",getComputerChoice()));
 
+function playGame(){
+    let countComputerWins=0;
+    let countPlayerWins=0;
+
+
+    for (let i=0; i<5;i++){//Playing a game of 5 rounds
+        let userChoice = prompt("enter your guess");
+        let result = playRound(userChoice, getComputerChoice());
+        
+        switch (result){
+            case 0: countComputerWins +=1;
+            break;
+            case 1: countPlayerWins +=1;
+            break;
+            default:
+            break;
+    
+        }
+    }
+
+    if (countComputerWins < countPlayerWins){
+        console.log(`The player wins with ${countPlayerWins} over computer's ${countComputerWins}`)
+    } else if (countComputerWins > countPlayerWins){
+        console.log(`The computer wins with ${countComputerWins} over player's ${countPlayerWins}`)
+    } else console.log("Its a tie!");
+    
+
+}
+playGame();
